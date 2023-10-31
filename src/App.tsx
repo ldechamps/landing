b8b4a3e3996807257b7394a50ab1c4f44e38bd17
WBCE_prod <WBCE_builder@prod>
2023-10-31 17:10:59 +0100

    feat: add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
 
+import {  RouterProvider, createBrowserRouter, redirect } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import { fakeAuthProvider } from "./auth";
+import Services from './components/Services';
+
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    loader() {
+      // Our root route always provides the user, if logged in
+      return { user: fakeAuthProvider.username };
+    },
+    Component: Homepage
+  },
+  {
+    path: "/services",
+    Component: Services
+  },
+  {
+    path: "/logout",
+    async action() {
+      // We signout in a "resource route" that we can hit from a fetcher.Form
+      await fakeAuthProvider.signout();
+      return redirect("/");
+    },
+  }
+]);
 
 function App() {
-  return (<Homepage />);
+  return (
+    <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
+  )
+
+  //return (<Homepage />);
 }
 
 export default App;
